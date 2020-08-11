dba165795eb14e8a468c41c1631f2be277901c17
lizuncong <2900170896@qq.com>
2020-08-11 19:49:14 +0800

    stash

--- components/like/index.js
+++ components/like/index.js
@@ -11,13 +11,17 @@ Component({
    * 组件的初始数据
    */
   data: {
-
+    like: true,
+    count: 9,
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
-
+    onClick: (e) => {
+      console.log('click....', e)
+      this.setData({ count: this.count + 1})
+    }
   }
 })
